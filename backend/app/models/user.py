9f46c5f98212de0e4ac9bc4581ae56ee89c4d24f
Yermehyaw <yermehyaw@gmail.com>
2024-12-18 02:36:24 +0100

    [Update]: User model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -8,18 +8,136 @@ MODULES:
     - uuid: uuid4 class
 
 """
-from typing import Union
-from pydantic import BaseModel
+from typing import (
+    Union,
+    Optional
+)
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    Field,
+    constr,
+    validator
+)
 from datetime import datetime
 from uuid import uuid4
 
-class UserCreate(BaseModel):
+
+# CREATE USER
+class UserSignup(BaseModel):
     """
     Users description class for creating a new user
 
     ATTRIBUTES:
         - name: str
         - email: str
-        - p_hash: str
+        - password: str
+        - created_at: str
+        - updated_at: str
+        - profile_pic: bytes
+        - bio: str
+        - skills: list
+        - friends: list
+        - collabees: list
+        - objs: list
+        - interests: list
+        - projects: list
+        - followers: list
+        - following: list
+        - language: str
+        - location: str
+        - timezone: str
 
     """
+    user_id: str = str(uuid4())
+    name: str = Field(..., min_length=1, max_length=100)
+    email: EmailStr
+    password: str = Field(..., min_length=8)
+    created_at: str = datetime.now().isoformat()
+    updated_at: str = datetime.now().isoformat()
+    profile_pic: Optional[bytes] = None
+    bio: Optional[str] = None
+    skills: Optional[list] = None
+    friends: Optional[list] = None
+    collabees: Optional[list] = None  # list of users the user is currently collaborating with
+    objs: Optional[list] = None
+    interests: Optional[list] = None
+    projects: Optional[list] = None
+    followers: Optional[list] = None
+    following: Optional[list] = None
+    language: Optional[str] = 'eng'
+    location: Optional[str] = None
+    timezone: Optional[str] = 'UTC'
+
+
+# RETRIEVE USER
+class UserRetrieve(BaseModel):
+    """
+    Users description class for retrieving a user for responses. Only non-sensitive data should be included.
+
+    ATTRIBUTES:
+        - user_id: str
+        - name: str
+        - email: str
+        - created_at: str
+        - profile_pic: bytes
+        - bio: str
+        - skills: list
+        - friends: list
+        - collabees: list
+        - objs: list
+        - interests: list
+        - projects: list
+        - followers: list
+        - following: list
+        - language: str
+        - location: str
+        - timezone: str
+
+    """
+    user_id: str = str(uuid4())
+    name: str = Field(..., min_length=1, max_length=100)
+    email: EmailStr
+    created_at: str = datetime.now().isoformat()
+    profile_pic: Optional[bytes] = None
+    bio: Optional[str] = None
+    skills: Optional[list] = None
+    friends: Optional[list] = None
+    collabees: Optional[list] = None  # list of users the user is currently collaborating with
+    objs: Optional[list] = None
+    interests: Optional[list] = None
+    projects: Optional[list] = None
+    followers: Optional[list] = None
+    following: Optional[list] = None
+    language: Optional[str] = 'eng'
+    location: Optional[str] = None
+    timezone: Optional[str] = 'UTC'
+
+
+# USER LOGIN
+class UserLogin(BaseModel):
+    """
+    Users description class for logging in a user
+
+    ATTRIBUTES:
+        - email: str
+        - password: str
+        - 
+
+    """
+    email: EmailStr
+    password: str = Field(..., min_length=8)
+
+
+# JWT Token RESPONSE
+class Token(BaseModel):
+    """
+    Token description class for responses
+
+    ATTRIBUTES:
+        - access_token: str
+        - token_type: str
+
+    """
+    access_token: str
+    token_type: str = 'bearer' 
