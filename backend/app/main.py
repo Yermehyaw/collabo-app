b7dfdaedeb82dbe88edd4cf112256f3b7fec0551
Yermehyaw <yermehyaw@gmail.com>
2024-12-19 23:51:49 +0100

    [Update]: regster home, auth and user routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,12 +6,19 @@ MODULES:
 
 """
 from fastapi import FastAPI
-from routes.auth import auth_router
+from routes.auth_routes import auth_router
+from routes.user_routes import user_router
 
 app = FastAPI()
 
 # Map routes the fastAPI routes module
-app.include_router(auth_router, tags=['Auth'])
+app.include_router(auth_router, prefix='/auth', tags=['Auth'])
+app.include_router(user_router, prefix='/user', tags=['User'])
+
+# Homepage
+app.get('/')
+async def root():
+    return {"message": "Welcome to the FastAPI app"}
 
 # Run the app via uvicorn in a shell terminal
 # uvicorn main:app --reload
