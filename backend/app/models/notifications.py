ac88f0abbe4b4b1015547f4984a7608eefba22ce
Eyakenojnr <eyakenojnr@gmail.com>
2024-12-22 00:55:27 +0100

    [Update:] Added a validator for `user_id` and changed `created_at` field to correctly handle `datetime` objects

--- backend/app/models/notifications.py
+++ backend/app/models/notifications.py
@@ -1,19 +1,23 @@
 """
-Notifiaction model for fastapi app
-
-MODULES:
-    - pydantic: BaseModel
+Notifications model.
 """
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 from typing import Optional
 from datetime import datetime
 
 class Notification(BaseModel):
     """
-    Defines a notification object
+    Defines a notification object.
     """
     user_id: str
-    type: str
+    type: str # ??Use Enum to define acceptable notification types??
     content: str
     is_read: Optional[bool] = False
-    created_at: Optional[datetime] = datetime.now().isoformat()
+    created_at: Optional[datetime] = Field(default_factory=datetime.now) # Ensures `created_at` is generated at runtime
+
+    @validator("user_id")
+    def validate_user_id(cls, value: str) -> str: # Custom validator
+        """Custom validator that validates `user_id` to ensure consistency and correctness."""
+        if len(value) != 24: # Simple validation for ObjectId length
+            raise ValueError("Invalid user_id format")
+        return value
