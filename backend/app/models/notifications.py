69cab775ce79b6c685cd61c8e3e14588ebab1a48
Eyakenojnr <eyakenojnr@gmail.com>
2024-12-21 20:26:57 +0100

    [Add]: add

---
+++ backend/app/models/notifications.py
@@ -0,0 +1,19 @@
+"""
+Notifiaction model for fastapi app
+
+MODULES:
+    - pydantic: BaseModel
+"""
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class Notification(BaseModel):
+    """
+    Defines a notification object
+    """
+    user_id: str
+    type: str
+    content: str
+    is_read: Optional[bool] = False
+    created_at: Optional[datetime] = datetime.utc.now()
