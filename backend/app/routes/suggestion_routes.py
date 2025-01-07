e3f0d8a7735c61e9c701fe14f9827a445e815ea8
Yermehyaw <yermehyaw@gmail.com>
2025-01-07 14:36:54 +0100

    [Fix]: Fix auth token validation with OAuthPasswordBearer

--- backend/app/routes/suggestion_routes.py
+++ backend/app/routes/suggestion_routes.py
@@ -3,6 +3,7 @@ Suggestions routes for user feed generation
 
 MODULES:
     - fastapi: APIRouter, Depends, HTTPException, status
+    - fastapi.security: OAuth2PasswordBearer
     - typing: List
     - services.suggestion_services: SuggestionServices
     - models.projects: ProjectResponse
@@ -14,17 +15,20 @@ from fastapi import (
     APIRouter, Depends,
     status, HTTPException
 )
+from fastapi.security import OAuth2PasswordBearer
 from typing import List
 from services.suggestion_services import SuggestionServices
-from app.models.projects import ProjectResponse
-from app.models.users import UserResponse
+from models.projects import ProjectResponse
+from models.users import UserResponse
 from utils.auth.jwt_handler import verify_access_token
 
 suggestion_router = APIRouter()
 suggestion_services = SuggestionServices()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 @suggestion_router.get("/projects/", response_model=List[ProjectResponse])
-async def get_project_feed(token: str = Depends(verify_access_token)):
+async def get_project_feed(token: str = Depends(oauth2_scheme)):
     """
     Route to get project suggestions for a user
 
@@ -34,11 +38,17 @@ async def get_project_feed(token: str = Depends(verify_access_token)):
     RETURNS:
         - List[ProjectResponse]: json list of project objects
     """
+    token = verify_access_token(token)
+    if not token:
+        failure = {"error": "Invalid token", "code": "UNAUTHORIZED"}
+        raise HTTPException(status_code=401, detail=failure)
+    
     user_id = token.get("sub")
     return await suggestion_services.get_project_suggestions(user_id)
 
+
 @suggestion_router.get("/users/", response_model=List[UserResponse])
-async def get_user_feed(token: str = Depends(verify_access_token)):
+async def get_user_feed(token: str = Depends(oauth2_scheme)):
     """
     Route to get user suggestions/recommendations for a collaboration
 
@@ -48,5 +58,10 @@ async def get_user_feed(token: str = Depends(verify_access_token)):
     RETURNS:
         - List[UserResponse]: json list of user objects
     """
+    token = verify_access_token(token)
+    if not token:
+        failure = {"error": "Invalid token", "code": "UNAUTHORIZED"}
+        raise HTTPException(status_code=401, detail=failure)
+
     user_id = token.get("sub")
     return await suggestion_services.get_user_suggestions(user_id)
