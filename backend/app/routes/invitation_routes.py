bf6f0033cc8285a3b2a04729820a19fcb1e52a69
Yermehyaw <yermehyaw@gmail.com>
2025-01-10 17:52:31 +0100

    [Fix]: Fix AttrErr, invalid methd call

--- backend/app/routes/invitation_routes.py
+++ backend/app/routes/invitation_routes.py
@@ -57,7 +57,7 @@ async def send_invitation(invite: InvitationCreate, token: str = Depends(oauth2_
         raise HTTPException(status_code=401, detail=failure)
 
     # Ensure its the project owner sending the request
-    project = await project_services.get_project(invite.project_id)
+    project = await project_services.get_project_by_id(invite.project_id)
 
     if not project or project["creator_id"] != token["sub"]:
         failure = {"error": "You are not permitted to send invites to other users on this project", "code": "PERMISSION_DENIED"}
