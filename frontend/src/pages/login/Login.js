25ca25e19d720fef522ebc26e936bc295473a9b1
Yermehyaw <yermehyaw@gmail.com>
2025-02-02 05:40:04 +0100

    [Fix]: Import err

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Login.css";
+import "./login.css";
 import Footer from "../../components/Footer/Footer";
 import { Link } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
